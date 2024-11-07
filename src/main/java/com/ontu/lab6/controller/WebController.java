a9376c9f2ca66c629d45f9aa6fa2fe7c9dce17b8
Magosusas <watmanovich56@gmail.com>
2024-11-07 21:43:47 +0200

    New added

--- src/main/java/com/ontu/lab6/controller/WebController.java
+++ src/main/java/com/ontu/lab6/controller/WebController.java
@@ -4,20 +4,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Random;
+
 @Controller
 public class WebController {
-    private final char[][] gameBoard;
+    
+    public static int SIZE = 3;
+    public Random random = new Random();
 
-    public WebController() {
-        gameBoard = new char[][] {
-            {'X', 'O', '-'},
-            {'-', 'X', 'O'},
-            {'O', '-', 'X'}
-        };
+    public char[][] generateRandomGameBoard() {
+        char[][] board = new char[SIZE][SIZE];
+        char[] symbols = {'X', 'O', '-'};
+
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                board[i][j] = symbols[random.nextInt(symbols.length)];
+            }
+        }
+        return board;
     }
 
     @RequestMapping(value = "/index")
     public String index(Model model) {
+        char[][] gameBoard = generateRandomGameBoard();
         model.addAttribute("gameBoard", gameBoard);
         return "index";
     }
